2a2c4e0b7f7ea1a5af650aef0e61265307cd444a
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-10-25 22:25:28 +0900

    Add sklearn version of test in kmeans_test

--- kmeans_test.py
+++ kmeans_test.py
@@ -11,7 +11,7 @@ X_train, X_test = train_test_split(X, test_size=0.33, random_state=42)
 clusters = len(np.unique(y))
 print('K is', clusters)
 
-k = KMeans(K=clusters, max_iters=150, plot_steps=True)
+k = KMeans(K=clusters, max_iters=150, plot_steps=False)
 k.fit(X_train)
 
 y_test = k.predict(X_test)
@@ -19,3 +19,32 @@ y_test = k.predict(X_test)
 print(y_test.shape)
 print(y_test)
 
+# for sklearn library
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from kneed import KneeLocator
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(X_train)
+
+k = KMeans(init="random", n_clusters=clusters, max_iter=150)
+k.fit(scaled_features)
+
+y_test = k.predict(X_test)
+
+print(y_test.shape)
+print(y_test)
+
+df = pd.DataFrame({"x": X_test[:,0], "y": X_test[:,1], "cluster": y_test})
+print('df', df)
+groups = df.groupby("cluster")
+
+for cluster, data in groups:
+    plt.scatter(data["x"], data["y"], label=cluster)
+plt.title('sklearn version of K-Means Clustering')
+plt.show()
