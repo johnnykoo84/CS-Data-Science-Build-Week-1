0e1fda60dc232087336c949ef397a740ce33f759
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-10-27 10:39:21 +0900

    Add StandardScalar

--- kmeans_test.py
+++ kmeans_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as pyplot
 from kmeans import KMeans
@@ -8,13 +9,17 @@ from kmeans import KMeans
 X, y = make_blobs(centers=4, n_samples=500, n_features=2, shuffle=True, random_state=42)
 X_train, X_test = train_test_split(X, test_size=0.33, random_state=42)
 
+scaler = StandardScaler()
+scaled_X_train = scaler.fit_transform(X_train)
+scaled_X_test = scaler.fit_transform(X_test)
+
 clusters = len(np.unique(y))
 print('K is', clusters)
 
 k = KMeans(K=clusters, max_iters=150, plot_steps=False)
-k.fit(X_train)
+k.fit(scaled_X_train)
 
-y_test = k.predict(X_test)
+y_test = k.predict(scaled_X_test)
 
 print(y_test.shape)
 print(y_test)
@@ -27,20 +32,17 @@ import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import StandardScaler
 
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(X_train)
+# k = KMeans(init="random", n_clusters=clusters, max_iter=150)
+k = KMeans(init="k-means++", n_clusters=clusters, max_iter=150)
+k.fit(scaled_X_train)
 
-k = KMeans(init="random", n_clusters=clusters, max_iter=150)
-k.fit(scaled_features)
-
-y_test = k.predict(X_test)
+y_test = k.predict(scaled_X_test)
 
 print(y_test.shape)
 print(y_test)
 
-df = pd.DataFrame({"x": X_test[:,0], "y": X_test[:,1], "cluster": y_test})
+df = pd.DataFrame({"x": scaled_X_test[:,0], "y": scaled_X_test[:,1], "cluster": y_test})
 print('df', df)
 groups = df.groupby("cluster")
 
